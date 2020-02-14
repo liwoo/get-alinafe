43eb5794613fe5827d6146489ed437a4a3911564
Jeremiah Chienda <jeremiahchienda@gmail.com>
2020-02-14 18:34:49 +0200

    test netlify functions

---
+++ javascript/mailer.js
@@ -0,0 +1,32 @@
+const mailer = require("nodemailer");
+
+const transporter = mailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "liwuraps@gmail.com",
+    pass: "t1m3ISn0w"
+  }
+});
+
+export default function send(recepient) {
+  const mailOptions = {
+    from: "Liwu <liwuraps@gmail.com>",
+    to: $recepient,
+    subject: "Enjoy the Intro.",
+    attachments: [
+      {
+        filename: "Intro.mp3.zip",
+        path: __dirname + "/Intro.mp3.zip"
+      }
+    ],
+    html:
+      "<p>Whatsup!</p><p><Thank you for supporting my music!</p><p>Please find attached the Intro to my upcoming album!  Don't forget to check out the video on YouTube as well.</p><br /><p>Much Appreciated,</p><p>Liwu</p>"
+  };
+  transporter.sendMail(mailOptions, function(error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent: " + info.response);
+    }
+  });
+}
